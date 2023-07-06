7455b910218fb5671c581af3308c10e061850525
EdgarAnt <eadv7777@gmail.com>
2023-07-05 19:11:44 -0600

    simpleSum

---
+++ helloWorld.cpp
@@ -0,0 +1,20 @@
+//My first program reading book of c++ Detitel&Deitel
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    
+    int integer1, integer2, sum; //declare variables
+ 
+    cout<<"what is the the first number"; // petition
+    cin>> integer1; //input
+    cout<<"what is the second number"; // petition
+    cin>> integer2; //input
+    sum = integer1 + integer2; //print the sum
+    cout<<"la suma es: "<<sum<<endl;
+    return 0; //This is for indicated that finished the program
+
+    
+}
